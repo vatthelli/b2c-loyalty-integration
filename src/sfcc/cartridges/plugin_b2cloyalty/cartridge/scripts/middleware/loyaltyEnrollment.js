91b0e4cd7ca0de40e9e4d9c1338420bc21cd92ba
Oleg Sapishchuk (Salesforce) <osapishchuk@salesforce.com>
2021-08-26 14:49:14 +1000

    Functionality for Loyalty B2C Commerce Cloud Integration : Loyalty Member Acquisition : Customer opt in and opt out  Created loyalty sub object structure under account object, created loyalty middlware for security, created form templates and controllers to optin and optuot from my account page. Added basic validation rules. TODO FD JS with handling POST requests.

---
+++ src/sfcc/cartridges/plugin_b2cloyalty/cartridge/scripts/middleware/loyaltyEnrollment.js
@@ -0,0 +1,95 @@
+'use strict';
+
+var URLUtils = require('dw/web/URLUtils');
+var Customer = require('dw/customer/Customer');
+
+/**
+ * Middleware validating if user enrolled into loyalty
+ * @param {Object} req - Request object
+ * @param {Object} res - Response object
+ * @param {Function} next - Next call in the middleware chain
+ * @returns {void}
+ */
+function validateLoyaltyEnrolled(req, res, next) {
+    var customer = req.currentCustomer;
+    var isLoyaltyCustomer = customer instanceof Customer ? customer.profile.custom.b2cloyalty_optInStatus : customer.raw.profile.custom.b2cloyalty_optInStatus
+    if (!isLoyaltyCustomer) {
+        res.redirect(URLUtils.url('Account-Show'));
+    }
+
+    next();
+}
+
+/**
+ * Middleware validating if user unregistered into loyalty
+ * @param {Object} req - Request object
+ * @param {Object} res - Response object
+ * @param {Function} next - Next call in the middleware chain
+ * @returns {void}
+ */
+function validateLoyaltyUnregistered(req, res, next) {
+    var customer = req.currentCustomer;
+    var isLoyaltyCustomer = customer instanceof Customer ? customer.profile.custom.b2cloyalty_optInStatus : customer.raw.profile.custom.b2cloyalty_optInStatus
+    if (isLoyaltyCustomer) {
+        res.redirect(URLUtils.url('Account-Show'));
+    }
+
+    next();
+}
+
+/**
+ * Middleware validating if user enrolled into loyalty from ajax request
+ * @param {Object} req - Request object
+ * @param {Object} res - Response object
+ * @param {Function} next - Next call in the middleware chain
+ * @returns {void}
+ */
+function validateLoyaltyEnrolledAjax(req, res, next) {
+    var customer = req.currentCustomer;
+    var isLoyaltyCustomer = customer instanceof Customer ? customer.profile.custom.b2cloyalty_optInStatus : customer.raw.profile.custom.b2cloyalty_optInStatus
+    if (!isLoyaltyCustomer) {
+        res.setStatusCode(500);
+        res.setViewData({
+            isLoyaltyCustomer: false,
+            redirectUrl: URLUtils.url('Account-Show').toString()
+        });
+    } else {
+        res.setViewData({
+            isLoyaltyCustomer: true
+        });
+    }
+
+    next();
+}
+
+/**
+ * Middleware validating if user enrolled into loyalty from ajax request
+ * @param {Object} req - Request object
+ * @param {Object} res - Response object
+ * @param {Function} next - Next call in the middleware chain
+ * @returns {void}
+ */
+function validateLoyaltyUnregisteredAjax(req, res, next) {
+    var customer = req.currentCustomer;
+    var isLoyaltyCustomer = customer instanceof Customer ? customer.profile.custom.b2cloyalty_optInStatus : customer.raw.profile.custom.b2cloyalty_optInStatus
+    if (isLoyaltyCustomer) {
+        res.setStatusCode(500);
+        res.setViewData({
+            isLoyaltyCustomer: true,
+            redirectUrl: URLUtils.url('Account-Show').toString()
+        });
+    } else {
+        res.setViewData({
+            isLoyaltyCustomer: false
+        });
+    }
+
+    next();
+}
+
+module.exports = {
+    validateLoyaltyEnrolled: validateLoyaltyEnrolled,
+    validateLoyaltyUnregistered: validateLoyaltyUnregistered,
+    validateLoyaltyEnrolledAjax: validateLoyaltyEnrolledAjax,
+    validateLoyaltyUnregisteredAjax: validateLoyaltyUnregisteredAjax
+};
